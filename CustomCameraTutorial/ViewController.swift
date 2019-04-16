0d633c7e9316a2112724f4c803c0b27b6f6e2a32
Josh Hawkins <hawkins@users.noreply.github.com>
2019-04-16 19:05:05 -0400

    Initial Commit

---
+++ CustomCameraTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCameraTutorial
+//
+//  Created by Josh Hawkins on 4/16/19.
+//  Copyright © 2019 Josh Hawkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
